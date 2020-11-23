29b79dc27ea572addcd2124806a46f22247622ae
Ben Muller <ben.k.muller@gmail.com>
2020-11-23 18:15:35 +1100

    my solution

--- source_code/door.py
+++
@@ -1,9 +0,0 @@
-class Door:
-    '''
-    This class specified one door object
-    '''
-    def __init__(self, object_name = None):
-        self._object_present_behind=object_name
-
-    def get_object_behind_door(self):
-        return self._object_present_behind
--- source_code/main.py
+++
@@ -1,8 +0,0 @@
-from source_code.monty_hall import Monty_Hall
-
-def main():
-    monty_hall=Monty_Hall(10000)
-    monty_hall.run_simulations()
-
-if __name__ == '__main__':
-    main()
